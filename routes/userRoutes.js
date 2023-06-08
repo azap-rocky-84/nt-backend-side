31bfafa178e41499a0d469eeaf61e94dc030a0f5
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-08 19:41:24 +0200

    user registration functionality

---
+++ routes/userRoutes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const router = express.Router();
+import { registerUser } from '../controllers/userControllers';
+router.post('/register', registerUser);
+export default router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 connectDB();
@@ -9,5 +10,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Il server va...");
 });
+app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Il server va sulla porta ${PORT}`));
