81416e0740d108de35dfc5bc0d73f88118c8e040
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-17 17:12:55 +0200

    return all posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import {createPost, deletePost, getPost, updatePost} from '../controllers/postControllers';
+import {createPost, deletePost, getAllPosts, getPost, updatePost} from '../controllers/postControllers';
 import { authGuard, adminGuard } from '../middleware/authMiddleware';
-router.post("/", authGuard, adminGuard, createPost);
+router.route("/").post(authGuard, adminGuard, createPost).get(getAllPosts);
 router.route("/:slug").put(authGuard, adminGuard, updatePost).delete(authGuard, adminGuard, deletePost).get(getPost);
 export default router;
