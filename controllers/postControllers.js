e1420ae6b2a4afc9a2d470c3a33f727cc9faf912
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-30 12:08:42 +0200

    search functionality

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -131,13 +131,35 @@ const getPost = async (req, res, next) =>{
 
 const getAllPosts = async (req, res, next) => {
     try {
-        const posts = await Post.find({}).populate([
+        const filter = req.query.searchKeyword;
+        let where = {};
+        if(filter){
+            where.title = {$regex: filter, $options: "i"};
+        }
+        let query = Post.find(where);
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.limit) || 10;
+        const skip = (page-1) * pageSize;
+        const total = await Post.countDocuments();
+        const pages = Math.ceil(total/pageSize);
+        if(page>pages){
+            const error = new Error("Nessuna pagina trovata");
+            return next(error);
+        }
+        const result = await query.skip(skip).limit(pageSize).populate([
             {
-                path: 'user',
-                select: ["avatar", "name", "verified"],
-            }
-        ]);
-        res.json(posts);
+                path: "user",
+                select: ['avatar', "name", "verified"],
+            },
+        ]).sort({updatedAt: 'desc'});
+        res.header({
+            'x-filter':  filter,
+            'x-totalcount': JSON.stringify(total),
+            'x-currentpage': JSON.stringify(page),
+            'x-pagesize': JSON.stringify(pageSize),
+            'x-totalpagecount': JSON.stringify(pages),
+        });
+        return res.json(result);
     } catch (error) {
         next(error);
     }
