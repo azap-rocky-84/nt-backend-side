863beca37a62a0aa55bf5a384411117b09ffa53a
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-08-09 10:14:14 +0200

    create some nt methods

--- controllers/nationalTeamsControllers.js
+++ controllers/nationalTeamsControllers.js
@@ -1,12 +1,141 @@
 import { uploadPicture } from "../middleware/uploadPictureMiddleware";
 import NationalTeams from "../models/NationalTeams";
+import { fileRemover } from "../utils/fileRemover";
 
 const createNt = async (req, res, next) => {
     try {
         const nt = new NationalTeams({
-            title: "titolo semplice"
+            title: "titolo",
+            body: {
+                type: "doc",
+                content: [],
+            },
+            emblem: "",
+            flag: "",
+            confederation: "confederazione",
+            fifaCode: "codiceFifa",
+            nickname: "nickname",
+            ct: "ct",
+            mostCappedPlayer: "presenze",
+            caps: 0,
+            topScorer: "cannoniere",
+            goals: 0
+        });
+        const createdNt = await nt.save();
+        return res.json(createdNt);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateNt = async (req, res, next) => {
+    try {
+        const nt = await NationalTeams.findOne({title: req.params.title});
+        if(!nt){
+            const error = new Error("Nazionale non trovata");
+            next(error);
+            return;
+        }
+        const uploadFlag = uploadPicture.single('flag');
+        const uploadEmblem = uploadPicture.single('emblem');
+        const handleUpdateNtData = async (data) => {
+            const {title, body, emblem, flag, confederation, fifaCode, nickname, ct, mostCappedPlayer, caps, topScorer, goals} = JSON.parse(data);
+            nt.title = title || nt.title;
+            nt.body = body || nt.body;
+            nt.confederation = confederation || nt.confederation;
+            nt.fifaCode = fifaCode || nt.fifaCode;
+            nt.nickname = nickname || nt.nickname;
+            nt.ct = ct || nt.ct;
+            nt.mostCappedPlayer = mostCappedPlayer || nt.mostCappedPlayer;
+            nt.caps = caps || nt.caps;
+            nt.topScorer = topScorer || nt.topScorer;
+            nt.goals = goals || nt.goals;
+            const updateNt = await nt.save();
+            return res.json(updateNt);
+        };
+        uploadFlag(req, res, async function(err){
+            if(err){
+                const error = new Error("Errore sconosciuto durante il caricamento della bandiera");
+                next(error);
+            } else {
+                if(req.file){
+                    let filename;
+                    filename = nt.flag;
+                    if(filename){
+                        fileRemover(filename);
+                    }
+                    nt.flag = req.file.filename;
+                    handleUpdateNtData(req.body.document);
+                } else {
+                    let filename;
+                    filename = nt.flag;
+                    nt.flag = "";
+                    fileRemover(filename);
+                    handleUpdateNtData(req.body.document);
+                }
+            }
+        });
+        uploadFlag(req, res, async function(err){
+            if(err){
+                const error = new Error("Errore sconosciuto durante il caricamento della bandiera");
+                next(error);
+            } else {
+                if(req.file){
+                    let filename;
+                    filename = nt.flag;
+                    if(filename){
+                        fileRemover(filename);
+                    }
+                    nt.flag = req.file.filename;
+                    handleUpdateNtData(req.body.document);
+                } else {
+                    let filename;
+                    filename = nt.flag;
+                    nt.flag = "";
+                    fileRemover(filename);
+                    handleUpdateNtData(req.body.document);
+                }
+            }
+        });
+        uploadEmblem(req, res, async function(err){
+            if(err){
+                const error = new Error("Errore sconosciuto durante il caricamento dello stemma");
+                next(error);
+            } else {
+                if(req.file){
+                    let filename;
+                    filename = nt.emblem;
+                    if(filename){
+                        fileRemover(filename);
+                    }
+                    nt.emblem = req.file.filename;
+                    handleUpdateNtData(req.body.document);
+                } else {
+                    let filename;
+                    filename = nt.emblem;
+                    nt.emblem = "";
+                    fileRemover(filename);
+                    handleUpdateNtData(req.body.document);
+                }
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteNt = async (req, res, next) =>{
+    try {
+        const nt = await NationalTeams.findOneAndDelete({title: req.params.title});
+        if(!nt){
+            const error = new Error("Nazionale non trovata");
+            return next(error);
+        }
+        return res.json({
+            message: "La nazionale è stata eliminata correttamente",
         })
     } catch (error) {
         next(error);
     }
-}
+};
+export {createNt, updateNt, deleteNt};
--- routes/nationalTeamsRoutes.js
+++ routes/nationalTeamsRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import {} from '../controllers/nationalTeamsControllers';
-router.post("/")
+import {createNt} from '../controllers/nationalTeamsControllers';
+import {authGuard, adminGuard} from '../middleware/authMiddleware';
+router.post("/", authGuard, adminGuard, createNt);
+
 export default router;
