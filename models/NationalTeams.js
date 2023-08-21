5891db0ed412f5db60011b6396e8956910b73c3c
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-08-21 14:11:38 +0200

    edit national teams logic

--- models/NationalTeams.js
+++ models/NationalTeams.js
@@ -2,16 +2,15 @@ import {Schema, model} from 'mongoose';
 const NationalTeamsSchema = new Schema({
     title: {type: String, required: true},
     body: {type: Object, required: true},
-    emblem: {type: String, required: false},
     flag: {type: String, required:false},
     confederation: {type: String, required:true},
     fifaCode: {type: String, required:true},
     nickname: {type: String, required:true},
     ct: {type: String, required:true},
-    mostCappedPlayer: {type: String, required:true},
-    caps: {type: Number, required:true},
-    topScorer: {type: String, required:true},
-    goals: {type: Number, required: true}
+    mostCappedPlayer: {type: [String]},
+    caps: {type: String, required:true},
+    topScorer: {type: [String]},
+    goals: {type: String, required: true}
 }, {timestamps: true, toJSON:{virtuals:true}}
 );
 const NationalTeams = model("NationalTeams", NationalTeamsSchema);
