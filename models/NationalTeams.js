d46c3dbfc8cccb792dfcebd17dad8aa2ea940e93
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-10-05 15:40:20 +0200

    prettier nt

--- models/NationalTeams.js
+++ models/NationalTeams.js
@@ -1,17 +1,19 @@
-import {Schema, model} from 'mongoose';
-const NationalTeamsSchema = new Schema({
-    title: {type: String, required: true},
-    body: {type: Object, required: true},
-    flag: {type: String, required:false},
-    confederation: {type: String, required:true},
-    fifaCode: {type: String, required:true},
-    nickname: {type: String, required:true},
-    ct: {type: String, required:true},
-    mostCappedPlayer: {type: [String]},
-    caps: {type: String, required:true},
-    topScorer: {type: [String]},
-    goals: {type: String, required: true}
-}, {timestamps: true, toJSON:{virtuals:true}}
+import { Schema, model } from "mongoose";
+const NationalTeamsSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: Object, required: true },
+    flag: { type: String, required: false },
+    confederation: { type: String, required: true },
+    fifaCode: { type: String, required: true },
+    nickname: { type: String, required: true },
+    ct: { type: String, required: true },
+    mostCappedPlayer: { type: [String] },
+    caps: { type: String, required: true },
+    topScorer: { type: [String] },
+    goals: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
 );
 const NationalTeams = model("NationalTeams", NationalTeamsSchema);
 export default NationalTeams;
